44d39e6e8d183a94c1bfa28781750022a1746f7c
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2016-10-26 23:56:03 -0600

    Conexión BD y Dal para los usuarios
    Creación de la clase para conectarse a la BD
    Creación de la capa de datos para los usuarios

---
+++ UniversidadTJV/Datos/ConexionBD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class ConexionBD
+    {
+        //Atributos
+        private SqlConnectionStringBuilder _conexionStr;
+        private SqlConnection _coneccionBD;
+        public SqlCommand instruccionSQL;
+        private SqlDataAdapter _sqlAdaptador;
+        private DataTable _datosConsulta;
+
+       public ConexionBD()
+        {
+            _conexionStr = new SqlConnectionStringBuilder();
+            _conexionStr.DataSource = "(local)";
+            _conexionStr.InitialCatalog = "UniversidadTJV";
+            _conexionStr.UserID = "sa";
+            _conexionStr.Password = "sa";
+        }
+
+        public void conectar()
+        {
+            _coneccionBD = new SqlConnection(_conexionStr.ConnectionString);
+            _coneccionBD.Open();
+        }
+
+        public void desconectar()
+        {
+            _coneccionBD.Close();
+        }
+
+        //Ejecutar un SP sin parámetros
+        public void sqlQuery(string sqlQuery)
+        {
+            instruccionSQL = new SqlCommand(sqlQuery, _coneccionBD);
+        }
+
+        //Ejecutar un SP con parámetros
+        public void sqlQuery(string storeProc, String[,] parametros)
+        {
+            instruccionSQL = new SqlCommand(storeProc, _coneccionBD);
+            instruccionSQL.CommandType = CommandType.StoredProcedure;
+            for (int i = 0; i < parametros.GetLength(0); i++)
+            {
+                instruccionSQL.Parameters.Add(new SqlParameter(parametros[i, 0], parametros[i, 1]));
+            }
+        }
+
+        public DataTable ejecutarConsultaSQL()
+        {
+            _sqlAdaptador = new SqlDataAdapter();
+            _sqlAdaptador.SelectCommand = instruccionSQL;
+            _datosConsulta = new DataTable();
+            _sqlAdaptador.Fill(_datosConsulta);
+            return _datosConsulta;
+        }
+
+    }
+}
---
+++ UniversidadTJV/Datos/dalUsuarios.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class dalUsuarios
+    {
+        entUsuarios usuarioEnt = new entUsuarios();
+        ConexionBD conexion = new ConexionBD();
+        SqlCommand cmd = new SqlCommand();
+        DataTable datos = new DataTable();
+        string mensaje;
+
+        //Métodos
+        public DataTable ListarUsuarios()
+        {
+            entUsuarios usuarios = new entUsuarios();
+            ConexionBD conexion = new ConexionBD();
+
+            try
+            {
+                conexion.conectar();
+                conexion.sqlQuery("sp_listado_usuarios");
+                datos = conexion.ejecutarConsultaSQL();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return datos;
+        }
+
+        public entUsuarios ConsultarUsuarios(int idUsuario)
+        {
+            try
+            {
+                conexion.conectar();
+                String[,] parametros = new String[1, 2] { { "@id_usuario", idUsuario.ToString() } };
+                conexion.sqlQuery("sp_consultar_usuario", parametros);
+                datos = conexion.ejecutarConsultaSQL();
+
+                //Se cargan los datos del usuario, de la BD a los atributos de la clase
+                if (datos.Rows.Count > 0)
+                {
+                    DataRow fila = datos.Rows[0];
+
+                    this.usuarioEnt.Nombre = fila["nombre"].ToString();
+                    this.usuarioEnt.Nombre = fila["cedula"].ToString();
+                    this.usuarioEnt.NombreUsuario = fila["nombre_usuario"].ToString();
+                    this.usuarioEnt.PrimerApellido = fila["primer_apellido"].ToString();
+                    this.usuarioEnt.SegundoApellido = fila["segundo_apellido"].ToString();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return this.usuarioEnt;
+        }
+
+        public string EliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                conexion.conectar();
+                String[,] parametros = new String[1, 2] { { "@id_usuario", idUsuario.ToString() } };
+                conexion.sqlQuery("sp_eliminar_usuario", parametros);
+                datos = conexion.ejecutarConsultaSQL();
+                mensaje = "El usuario fue eliminado";
+            }
+            catch (SqlException ex)
+            {
+                mensaje = "El usuario no pudo ser eliminado";
+                Console.WriteLine(ex);
+            }
+            return mensaje;
+        }
+
+        public string AgregarUsuario(string cedula, string nombre_usuario, string contrasena, string nombre, string primer_apellido, string segundo_apellido, int usuario_ingresa, int usuario_modifica)
+        {
+            try
+            {
+                conexion.conectar();
+                String[,] parametros = new String[8, 2] {{  "@cedula", cedula},
+                                                         {  "@nombre_usuario", nombre_usuario},
+                                                         {  "@contrasena", contrasena},
+                                                         {  "@nombre",nombre},
+                                                         {  "@primer_apellido", primer_apellido},
+                                                         {  "@segundo_apellido", segundo_apellido} ,
+                                                         {  "@usuario_ingresa", usuario_ingresa.ToString() },
+                                                         {  "@usuario_modifica", usuario_modifica.ToString()}};
+                conexion.sqlQuery("sp_agregar_usuario", parametros);
+                datos = conexion.ejecutarConsultaSQL();
+                mensaje = "El usuario fue ingresado";
+            }
+            catch (SqlException ex)
+            {
+                mensaje = "El usuario no pudo ser ingresado";
+                Console.WriteLine(ex);
+            }
+            return mensaje;
+        }
+
+        public string ModificarUsuario(string cedula, string nombre_usuario, string contrasena, string nombre, string primer_apellido, string segundo_apellido, int usuario_modifica)
+        {
+            try
+            {
+                conexion.conectar();
+                String[,] parametros = new String[7, 2] {{  "@cedula", cedula},
+                                                         {  "@nombre_usuario", nombre_usuario},
+                                                         {  "@contrasena", contrasena},
+                                                         {  "@nombre",nombre},
+                                                         {  "@primer_apellido", primer_apellido},
+                                                         {  "@segundo_apellido", segundo_apellido} ,
+                                                         {  "@usuario_modifica", usuario_modifica.ToString()}};
+                conexion.sqlQuery("sp_modificar_usuario", parametros);
+                datos = conexion.ejecutarConsultaSQL();
+                mensaje = "El usuario fue modificado";
+            }
+            catch (SqlException ex)
+            {
+                mensaje = "El usuario no pudo ser modificado";
+                Console.WriteLine(ex);
+            }
+            return mensaje;
+        }
+
+        public entUsuarios BuscarUsuarios(string valor)
+        {
+            try
+            {
+                conexion.conectar();
+                valor = "%" + valor + "%";
+                String[,] parametros = new String[1, 2] { { "@valor", valor } };
+                conexion.sqlQuery("sp_buscar_usuario", parametros);
+                datos = conexion.ejecutarConsultaSQL();
+
+                //Se cargan los datos del usuario, de la BD a los atributos de la clase
+                if (datos.Rows.Count > 0)
+                {
+                    DataRow fila = datos.Rows[0];
+
+                    this.usuarioEnt.Nombre = fila["nombre"].ToString();
+                    this.usuarioEnt.Nombre = fila["cedula"].ToString();
+                    this.usuarioEnt.NombreUsuario = fila["nombre_usuario"].ToString();
+                    this.usuarioEnt.PrimerApellido = fila["primer_apellido"].ToString();
+                    this.usuarioEnt.SegundoApellido = fila["segundo_apellido"].ToString();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return this.usuarioEnt;
+        }
+    }
+}
