70d9a2425c87e3bdaa218fa438c6f44fe242c998
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2016-10-29 15:35:57 -0600

    Listado de Lugares
    Crear el listado de los lugares.

---
+++ UniversidadTJV/Datos/dalLugares.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class dalLugares
+    {
+        entLugares lugarEnt = new entLugares();
+        ConexionBD conexion = new ConexionBD();
+        SqlCommand cmd = new SqlCommand();
+        bool mensaje;
+
+        //Métodos
+        public DataTable ListarLugares()
+        {
+            DataTable datos = new DataTable();
+            entLugares lugares = new entLugares();
+            ConexionBD conexion = new ConexionBD();
+
+            try
+            {
+                conexion.conectar();
+                conexion.sqlQuery("sp_listado_Usuarios");
+                datos = conexion.ejecutarConsultaSQL();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return datos;
+        }
+    }
+}
---
+++ UniversidadTJV/Negocio/negLugares.cs
@@ -0,0 +1,15 @@
+﻿using Datos;
+using System.Data;
+
+namespace Negocios
+{
+    public class negLugares
+    {
+        dalLugares _LugaresDatos = new dalLugares();
+
+        public DataTable ListarLugares()
+        {
+            return _LugaresDatos.ListarLugares();
+        }
+    }
+}
